9d6deeae424346c78e6b014bdbeca491f63e0e47
infobarbosa <infobarbosa@yahoo.com.br>
2017-08-02 00:10:33 -0300

    Inclusão numa rede de testes denominada bigdata e ajuste do docker run para contemplar a nova configuração.

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,7 @@
 #Comando RUN do docker com as variaveis para setup do zookeeper
-docker run --ip 172.17.0.2 infobarbosa/zookeeper 
+docker run -d \
+       --net bigdata \
+       --ip 172.18.0.2 \
+       --name zookeeper \
+       infobarbosa/zookeeper:standalone 
 
