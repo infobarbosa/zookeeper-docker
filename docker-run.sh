1e08028e7af93d0d093604ad1ab43753f263eb92
infobarbosa <infobarbosa@yahoo.com.br>
2017-08-02 23:00:26 -0300

    ajustando o ip estático, entre outras coisas

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,7 @@
 #Comando RUN do docker com as variaveis para setup do zookeeper
 docker run -d \
        --net bigdata \
-       --ip 172.18.0.2 \
+       --ip 172.18.0.3 \
        --name zookeeper \
        infobarbosa/zookeeper:standalone 
 
